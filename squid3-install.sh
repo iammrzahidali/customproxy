6d8dc6af9f789666d0fcfb44ffc705a45bf095c0
iammrzahidali <iammrzahidali@gmail.com>
2024-12-02 22:21:04 +0500

    Update squid3-install.sh

--- squid3-install.sh
+++ squid3-install.sh
@@ -6,7 +6,7 @@ if cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 24.04"; then
     touch /etc/squid/passwd
     /bin/rm -f /etc/squid/squid.conf
     /usr/bin/touch /etc/squid/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/zdenekslavik/CreateProxy/master/squid.conf
+    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/iammrzahidali/customproxy/refs/heads/main/squid.conf
     /sbin/iptables -I INPUT -p tcp --dport 35550 -j ACCEPT
     /sbin/iptables-save
     systemctl enable squid
@@ -17,7 +17,7 @@ elif cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 20.04"; then
     touch /etc/squid/passwd
     /bin/rm -f /etc/squid/squid.conf
     /usr/bin/touch /etc/squid/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/zdenekslavik/CreateProxy/master/squid.conf
+    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/iammrzahidali/customproxy/refs/heads/main/squid.conf
     /sbin/iptables -I INPUT -p tcp --dport 35550 -j ACCEPT
     /sbin/iptables-save
     systemctl enable squid
@@ -28,7 +28,7 @@ elif cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 18.04"; then
     touch /etc/squid/passwd
     /bin/rm -f /etc/squid/squid.conf
     /usr/bin/touch /etc/squid/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/zdenekslavik/CreateProxy/master/squid.conf
+    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/iammrzahidali/customproxy/refs/heads/main/squid.conf
     /sbin/iptables -I INPUT -p tcp --dport 35550 -j ACCEPT
     /sbin/iptables-save
     service squid restart
@@ -39,7 +39,7 @@ elif cat /etc/os-release | grep PRETTY_NAME | grep "Ubuntu 16.04"; then
     touch /etc/squid/passwd
     /bin/rm -f /etc/squid/squid.conf
     /usr/bin/touch /etc/squid/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/zdenekslavik/CreateProxy/master/squid.conf
+    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/iammrzahidali/customproxy/refs/heads/main/squid.conf
     /sbin/iptables -I INPUT -p tcp --dport 35550 -j ACCEPT
     /sbin/iptables-save
     service squid restart
@@ -50,7 +50,7 @@ elif cat /etc/*release | grep DISTRIB_DESCRIPTION | grep "Ubuntu 14.04"; then
     touch /etc/squid3/passwd
     /bin/rm -f /etc/squid3/squid.conf
     /usr/bin/touch /etc/squid3/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid3/squid.conf https://raw.githubusercontent.com/zdenekslavik/CreateProxy/master/squid.conf
+    /usr/bin/wget --no-check-certificate -O /etc/squid3/squid.conf https://raw.githubusercontent.com/iammrzahidali/customproxy/refs/heads/main/squid.conf
     /sbin/iptables -I INPUT -p tcp --dport 35550 -j ACCEPT
     /sbin/iptables-save
     service squid3 restart
@@ -65,7 +65,7 @@ elif cat /etc/os-release | grep PRETTY_NAME | grep "jessie"; then
     touch /etc/squid3/passwd
     /bin/rm -f /etc/squid3/squid.conf
     /usr/bin/touch /etc/squid3/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid3/squid.conf https://raw.githubusercontent.com/zdenekslavik/CreateProxy/master/squid.conf
+    /usr/bin/wget --no-check-certificate -O /etc/squid3/squid.conf https://raw.githubusercontent.com/iammrzahidali/customproxy/refs/heads/main/squid.conf
     /sbin/iptables -I INPUT -p tcp --dport 35550 -j ACCEPT
     /sbin/iptables-save
     service squid3 restart
@@ -79,7 +79,7 @@ elif cat /etc/os-release | grep PRETTY_NAME | grep "stretch"; then
     touch /etc/squid/passwd
     /bin/rm -f /etc/squid/squid.conf
     /usr/bin/touch /etc/squid/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/zdenekslavik/CreateProxy/master/squid.conf
+    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/iammrzahidali/customproxy/refs/heads/main/squid.conf
     /sbin/iptables -I INPUT -p tcp --dport 35550 -j ACCEPT
     /sbin/iptables-save
     systemctl enable squid
@@ -92,7 +92,7 @@ elif cat /etc/os-release | grep PRETTY_NAME | grep "buster"; then
     touch /etc/squid/passwd
     /bin/rm -f /etc/squid/squid.conf
     /usr/bin/touch /etc/squid/blacklist.acl
-    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/zdenekslavik/CreateProxy/master/squid.conf
+    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/iammrzahidali/customproxy/refs/heads/main/squid.conf
     /sbin/iptables -I INPUT -p tcp --dport 35550 -j ACCEPT
     /sbin/iptables-save
     systemctl enable squid
